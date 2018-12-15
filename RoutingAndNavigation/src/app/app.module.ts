65ddb183693fb75628ef82021b6b9ea5ae000b10
tanishraj <tanishraj.quitegrowth@gmail.com>
2018-12-15 15:27:48 +0530

    GitHub Profile component added

--- RoutingAndNavigation/src/app/app.module.ts
+++ RoutingAndNavigation/src/app/app.module.ts
@@ -3,18 +3,25 @@ import { HttpModule } from '@angular/http';
 import { PostsService } from './services/posts.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    GithubProfileComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'posts', component:PostsComponent},
+      
+    ])
   ],
   providers: [
     PostsService,
